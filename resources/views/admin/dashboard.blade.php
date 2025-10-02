c2d8918a686cfd722e6bd001f1446d302d77e21e
gaelfvillarroel-eng <gael.f.villarroel@gmail.com>
2025-10-01 21:50:16 -0400

    solucionado el dash

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,16 +1,16 @@
 <x-admin-layout :breadcrumbs="[
     [
         'name' => 'Dashboard',
-        'route' => route('admin.dashboard')
+        'route' => route('admin.dashboard'),
     ],
-
 ]">
 
     <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
 
         <div class="bg-white rounded-lg shadow-lg p-6">
-             <div class="flex items-center">
-                <img class="h-8 w-8 rounded-full object-cover" src="{{ auth()->user()->profile_photo_url }}" alt="{{ auth()->user()->name }}" />
+            <div class="flex items-center">
+                <img class="h-8 w-8 rounded-full object-cover" src="{{ auth()->user()->profile_photo_url }}"
+                    alt="{{ auth()->user()->name }}" />
 
                 <div class="ml-4 flex-1">
                     <h1 class="text-lg font-semibold">
@@ -18,7 +18,7 @@
                     </h1>
                     <p class="text-lg">Desde este panel podras administrar tu tienda</p>
 
-                    <form action="{{route('logout')}}" method="POST">
+                    <form action="{{ route('logout') }}" method="POST">
                         @csrf
                         <button class="text-sm hover:text-blue-500">
                             Cerrar Sesión
@@ -26,10 +26,9 @@
 
 
                     </form>
-             </div>
+                </div>
+            </div>
         </div>
-
-    </div>
 
         <div class="bg-white rounded-lg shadow-lg p-6 flex items-center justify-center">
 
@@ -37,7 +36,7 @@
                 Coders Free
             </h2>
 
-
+        </div>
 
     </div>
 
